6c4352dfacaec79e7308fda64b0d3f73a7bd8479
Himanshu Panwar <Himanshu_Panwar@Dell.com>
2022-12-05 20:02:25 +0530

    second commit

---
+++ src/Catalog.API/Controllers/CatalogController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Catalog.API.Controllers
+{
+    [ApiController]
+    [Route("api/v1/[controller]")]
+    public class CatalogController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ src/Catalog.API/Entities/Product.cs
@@ -0,0 +1,61 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+using MongoDB.Bson;
+
+namespace Catalog.API.Entities
+{
+    public class Product
+    {
+        private string _id = String.Empty;
+        private string _name = String.Empty;
+        private string _category = String.Empty ;
+        private string _summary = String.Empty ;
+        private string _description = String.Empty ;
+        private string _imageFile = String.Empty ;
+        private decimal _price = Decimal.Zero;
+
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id
+        {
+            get => _id;
+            set => _id = value;
+        }
+
+        [BsonElement("Name")]
+        public string Name
+        {
+            get => _name;
+            set => _name = value;
+        }
+
+        public string Category
+        {
+            get => _category;
+            set => _category = value;
+        }
+
+        public string Summary
+        {
+            get => _summary;
+            set => _summary = value;
+        }
+
+        public string Description
+        {
+            get => _description;
+            set => _description = value;
+        }
+
+        public string ImageFile
+        {
+            get => _imageFile;
+            set => _imageFile = value;
+        }
+
+        public decimal Price
+        {
+            get => _price;
+            set => _price = value;
+        }
+    }
+}
